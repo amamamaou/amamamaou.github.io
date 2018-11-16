efd709ff631d376654a407dfc4a130baf2e30f9c
amamamaou <41187086+amamamaou@users.noreply.github.com>
2018-11-16 23:16:46 +0900

    仮のトップページ追加

---
+++ js/konami.js
@@ -0,0 +1,39 @@
+/*! Konami Command! */
+{
+  const
+    command = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+    {documentElement: html, body} = document;
+
+  let
+    standby = true,
+    index = 0,
+    timer = null;
+
+  document.addEventListener('keydown', ev => {
+    clearTimeout(timer);
+
+    if (standby && ev.keyCode === command[index++]) {
+      if (index >= command.length) {
+        const center = window.pageYOffset + window.innerHeight / 2;
+
+        standby = false;
+        index = 0;
+
+        html.style.overflowX = 'hidden';
+        body.style.pointerEvents = 'none';
+        body.style.transformOrigin = `50% ${center}px`;
+
+        body.addEventListener('animationend', () => {
+          standby = true;
+          html.style.cssText = body.style.cssText = body.className = '';
+        }, {once: true});
+
+        body.className = 'rotate';
+      } else {
+        timer = setTimeout(() => { index = 0; }, 2000);
+      }
+    } else {
+      index = 0;
+    }
+  });
+}
