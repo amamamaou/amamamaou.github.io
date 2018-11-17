c222206a5c3f88061e050f7e9483e80c2b04bf44
amamamaou <41187086+amamamaou@users.noreply.github.com>
2018-11-17 19:43:16 +0900

    ツールの追加及び修正

---
+++ js/data-uri.js
@@ -0,0 +1,69 @@
+/* data-uri.js | v1.0.0 | MIT License */
+{
+  const
+    dropArea = new Vue({
+      el: '#dropArea',
+      data: {
+        isOver: false,
+        isWait: false,
+      },
+      methods: {
+        dragover(ev) {
+          ev.dataTransfer.dropEffect = 'copy';
+          this.isOver = true;
+        },
+        readFile(ev) {
+          const file = ev.dataTransfer.files[0];
+          file && convert(file);
+          this.isOver = false;
+        },
+        change(ev) {
+          const file = ev.target.files[0];
+          ev.target.value = '';
+          file && convert(file);
+        },
+      },
+    }),
+    output = new Vue({
+      el: '#output',
+      data: {
+        fileInfo: '',
+        result: '',
+      },
+      methods: {
+        clear() { this.fileInfo = this.result = ''; },
+      },
+    });
+
+  const
+    sub = ['K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'],
+    filesize = bytes => {
+      const
+        exp = Math.log(bytes) / Math.log(1024) | 0,
+        size = bytes / Math.pow(1024, exp),
+        unit = exp === 0 ? 'bytes' : sub[exp - 1] + 'B';
+      return (exp === 0 ? size : size.toFixed(2)) + ' ' + unit;
+    };
+
+  const readFile = file => new Promise(resolve => {
+    const reader = new FileReader;
+    reader.onload = () => resolve(reader.result);
+    reader.readAsDataURL(file);
+  });
+
+  const convert = async file => {
+    if (file.size === 0) {
+      output.result = '';
+      return;
+    }
+
+    dropArea.isWait = true;
+
+    const result = await readFile(file);
+
+    output.fileInfo = `${file.name} (${filesize(file.size)})`;
+    output.result = result;
+
+    dropArea.isWait = false;
+  };
+}
---
+++ js/svg2png.js
@@ -0,0 +1,202 @@
+/*! svg2png.js | v1.0.0 | MIT License */
+{
+  const
+    maxSize = 20971520,  // 20MB
+    imageError = 'ブラウザが対応していないフォーマットです。';
+
+  let blobURL = null, enabled = true;
+
+  const dropReset = () => {
+    if (blobURL) {
+      URL.revokeObjectURL(blobURL);
+      blobURL = null;
+    }
+
+    output.isReset = true;
+    output.height = 0;
+    output.message = '';
+    output.image = '';
+  };
+
+  // instance
+  const
+    scaleBlock = new Vue({
+      el: '.scaleBlock',
+      data: {
+        type: 'relative',
+        scale: '1',
+        width: '',
+        height: '',
+      },
+      methods: {dropReset},
+    }),
+    dropArea = new Vue({
+      el: '.dropContent',
+      data: {
+        isOver: false,
+        isWait: false,
+      },
+      methods: {
+        dragover(ev) {
+          if (enabled) {
+            ev.dataTransfer.dropEffect = 'copy';
+            this.isOver = true;
+          }
+        },
+        readFile(ev) {
+          const file = ev.dataTransfer.files[0];
+          file && readFile(file);
+          this.isOver = false;
+        },
+        change(ev) {
+          const file = ev.target.files[0];
+          ev.target.value = '';
+          file && readFile(file);
+        },
+      },
+    }),
+    output = new Vue({
+      el: '#output',
+      data: {
+        isReset: true,
+        height: 0,
+        message: '',
+        image: '',
+        fileName: '',
+      },
+    });
+
+  output.$on('slideDown', () => {
+    output.isReset = false;
+    output.$nextTick(() => {
+      output.height = output.$el.children[0].offsetHeight;
+    });
+  });
+
+  const viewError = text => {
+    output.message = text;
+    output.$emit('slideDown');
+    dropArea.isWait = false;
+    enabled = true;
+  };
+
+  const readFile = async file => {
+    dropArea.isWait = true;
+    output.isReset = true;
+    output.height = 0;
+    output.message = '';
+    output.image = '';
+
+    enabled = false;
+
+    if (blobURL) {
+      URL.revokeObjectURL(blobURL);
+      blobURL = null;
+    }
+
+    if (!file.type.includes('image/')) {
+      viewError(imageError);
+    } else if (file.size > maxSize) {
+      viewError('ファイルサイズが3MBを超えています！');
+    } else {
+      const uri = await new Promise((resolve, reject) => {
+        const reader = new FileReader;
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+      }).catch(() => null);
+
+      if (uri) {
+        const image = new Image;
+        image.onload = () => buildImage(image, file.name);
+        image.onerror = () => viewError(imageError);
+        image.src = uri;
+      } else {
+        viewError('ファイルの読み込みに失敗しました');
+      }
+    }
+  };
+
+  const blob2URL = canvas => {
+    if (canvas.toBlob) {
+      return new Promise(resolve => {
+        canvas.toBlob(blob => {
+          blobURL = URL.createObjectURL(blob);
+          resolve(blobURL);
+        });
+      });
+    }
+
+    if (canvas.msToBlob) {
+      blobURL = URL.createObjectURL(canvas.msToBlob());
+      return blobURL;
+    }
+
+    return canvas.toDataURL();
+  };
+
+  const buildImage = async (source, name) => {
+    const
+      canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d'),
+      image = document.createElement('img'),
+      scale = parseFloat(scaleBlock.scale) || 1,
+      optWidth = parseFloat(scaleBlock.width),
+      optHeight = parseFloat(scaleBlock.height);
+
+    let
+      {naturalWidth, naturalHeight} = source,
+      ratio = naturalWidth / (naturalHeight || 1),
+      width, height;
+
+    output.fileName =  name.replace(/\.\w+$/, '.png');
+
+    if (naturalWidth === 0 || naturalHeight === 0) {
+      document.body.appendChild(source);
+      naturalWidth = source.naturalWidth || source.offsetWidth;
+      naturalHeight = source.naturalHeight || source.offsetHeight;
+      ratio = naturalWidth / naturalHeight;
+      source.remove();
+    }
+
+    if (naturalWidth === 0 || naturalHeight === 0) {
+      viewError(imageError);
+      return;
+    }
+
+    if (scaleBlock.type === 'size') {
+      width = naturalWidth * scale;
+      height = naturalHeight * scale;
+    } else {
+      if (!isNaN(optWidth) && !isNaN(optHeight)) {
+        width = optWidth;
+        height = optHeight;
+      } else if (!isNaN(optWidth)) {
+        width = optWidth;
+        height = optWidth / ratio;
+      } else if (!isNaN(optHeight)) {
+        width = optHeight * ratio;
+        height = optHeight;
+      } else {
+        width = naturalWidth;
+        height = naturalHeight;
+      }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(source, 0, 0, width, height);
+
+    const url = await blob2URL(canvas);
+
+    image.onload = function () {
+      output.image = url;
+      output.$emit('slideDown');
+      dropArea.isWait = false;
+      enabled = true;
+    };
+
+    image.src = url;
+  };
+}
